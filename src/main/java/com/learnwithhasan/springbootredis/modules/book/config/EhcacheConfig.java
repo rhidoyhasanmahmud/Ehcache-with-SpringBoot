7c096569cc739a5571d7c54ad10b22b9f893f21c
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2021-01-05 22:42:54 +0600

    Ehcache implemented

---
+++ src/main/java/com/learnwithhasan/springbootredis/modules/book/config/EhcacheConfig.java
@@ -0,0 +1,25 @@
+package com.learnwithhasan.springbootredis.modules.book.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+
+@Configuration
+public class EhcacheConfig {
+
+    @Bean
+    CacheManager cacheManager(){
+        return new EhCacheCacheManager(ehCacheManager());
+    }
+
+    private net.sf.ehcache.CacheManager ehCacheManager() {
+        EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+        factoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        factoryBean.setShared(true);
+        return factoryBean.getObject();
+    }
+}
